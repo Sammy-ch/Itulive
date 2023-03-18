372f91d5bedb6f002f5eb3a411a9808594046572
Sammy <sammystcherubin@gmail.com>
2023-03-19 00:28:30 +0200

    Changed Video Source

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     
     <section className={`relative w-full h-screen max-autob`}>
-      <video  type="video/mp4" autoPlay loop muted playsInline poster='src/assets/herovid/vid_poster.png' width="320" height="240"
+      <video type="video/mp4" autoPlay loop muted playsInline poster='/vid_poster.png' width="320" height="240"
         class="video-docker absolute top-0 left-0 w-full h-full overflow-hidden object-cover">
         <source src='/herovid.mp4'></source>
       </video>
